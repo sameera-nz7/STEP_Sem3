55a52a670bab10f3f734e1120ba44bf12ca650d2
sameera-nz7 <naazsameera.i7@gmail.com>
2026-09-19 16:08:07 +0530

    Add Week 5 Assignment Q4

---
+++ Week5/Assignment/Q4/Main.java
@@ -0,0 +1,59 @@
+class LibraryMember {
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswer;
+
+    public LibraryMember() {
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String id) {
+        if (membershipId == null) {
+            membershipId = id;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premium) {
+        this.premiumMember = premium;
+    }
+
+    public void setSecurityAnswer(String answer) {
+        if (answer != null) {
+            securityAnswer = Integer.toHexString(answer.hashCode());
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LibraryMember m = new LibraryMember();
+
+        m.setMembershipId("LIB-8841");
+        m.setName("Priya Nair");
+        m.setPremiumMember(true);
+
+        System.out.println(m.getMembershipId());
+        System.out.println(m.isPremiumMember());
+
+        m.setMembershipId("FAKE-0000");
+        System.out.println(m.getMembershipId());
+
+        m.setSecurityAnswer("BlueMountain");
+    }
+}
