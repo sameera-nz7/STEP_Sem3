7b67e6645b87c1d32c5e5ff5b35239b3be07708d
sameera-nz7 <naazsameera.i7@gmail.com>
2026-09-19 16:02:44 +0530

    Add Week 5 Assignment Q3

---
+++ Week5/Assignment/Q3/Main.java
@@ -0,0 +1,45 @@
+class BookInventory {
+    private int copiesTotal;
+    private int copiesAvailable;
+
+    BookInventory(int copiesTotal) {
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    void checkOut() {
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    void checkIn() {
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BookInventory b = new BookInventory(3);
+
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+
+        System.out.println(b.getCopiesAvailable());
+
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+
+        System.out.println(b.getCopiesAvailable());
+    }
+}
